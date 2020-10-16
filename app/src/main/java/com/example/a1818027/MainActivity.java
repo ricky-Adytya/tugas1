220c2fcb0044809d36282f18445bc9a609694443
Ricky adytya <jrs.jrs225@gmail.com>
2020-10-16 20:19:17 +0700

    Ricky

---
+++ app/src/main/java/com/example/a1818027/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.a1818027;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void profile(View view){
+        Intent a = new Intent(MainActivity.this,
+                Profile.class);
+        startActivity(a);
+    }
+
+}
